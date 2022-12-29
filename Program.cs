2e86a31283eeeb07c5453f0f5ce5dba0d61780d4
E472-lab <114472539+E472-lab@users.noreply.github.com>
2022-12-29 15:56:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, y, digit, n1, n2;
+            Console.WriteLine("enter first number");
+            n1  = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second number");
+             n2 = int.Parse(Console.ReadLine());
+            num = n1;
+            while (num <= n2)
+            {
+                digit = 0;
+                for (y = 2; y < num; y++)
+                {
+                    if (num % y == 0)
+                    {
+                        digit = 1;
+                        break;
+                    }
+                }
+                if(digit==0)
+
+                    Console.WriteLine("number:"+num);
+                num++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
